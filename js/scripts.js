ee1258ef3403db3e0f98722c37b9825070f14eed
Emily <emily.guan27@hotmail.com>
2021-11-17 12:54:22 +1000

    Add functionality to hide specific text

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 			if (!tag_info.includes("Supplementary") &&
 				!unique_tags.includes(tag_info)) {
 				unique_tags.push(tag_info);
-	    		$('.list-group').append('<li class="list-group-item">' + tag_info + '</li>');
+	    		$('.lessons-list').append('<li class="list-group-item lessons-item">' + tag_info + '</li>');
 			}
 		};
 	    console.log(unique_tags);
@@ -19,7 +19,7 @@ $(document).ready(function() {
 
 	// Select lesson functionality
 	lessons = []
-	$(document).on('click', '.list-group-item', function() {
+	$(document).on('click', '.lessons-item', function() {
 		$(this).toggleClass('active');
 		const index = lessons.indexOf($(this).text());
 		if (index > -1) {
@@ -29,32 +29,56 @@ $(document).ready(function() {
 		}
 	});
 
+	// Select options to hide functionality
+	hidden = ['Character'];
+	$(document).on('click', '.hidden-item', function() {
+		$(this).toggleClass('active');
+		const index = hidden.indexOf($(this).text());
+		if (index > -1) {
+			hidden.splice(index, 1);
+		} else {
+			hidden.push($(this).text());
+		}
+		$("#reveal-" + $(this).text().toLowerCase()).toggleClass("reveal");
+	});
+
 	// Create list of characters of only necessary lessons
-	$('.btn-primary').click(function(){
-		characters = [];
-		for (var i = 0; i < all_characters.length; i++) {
-			tag_info = all_characters[i]["Tags"];
-			if (!tag_info.includes("Supplementary") &&
-				lessons.includes(tag_info.substring(0,22))) {
-				characters.push([all_characters[i]["Pinyin"],all_characters[i]["English"],all_characters[i]["Hanzi"]]);
-			}
-		};
+	$('.btn-primary').click(function() {
+		if (lessons.length == 0) {
+			alert("Please select atleast one lesson");
+		} else {
+			characters = [];
+			for (var i = 0; i < all_characters.length; i++) {
+				tag_info = all_characters[i]["Tags"];
+				if (!tag_info.includes("Supplementary") &&
+					lessons.includes(tag_info.substring(0,22))) {
+					characters.push([all_characters[i]["Pinyin"],all_characters[i]["English"],all_characters[i]["Hanzi"]]);
+				}
+			};
 
-		// Refresh page to character quiz	
-		$("#landing").hide();
-		$("#content").show();
-		total_characters = characters.length;
-		$("h3").text("00/" + total_characters);
+			// Refresh page to character quiz	
+			$("#landing").hide();
+			$("#content").show();
+			total_characters = characters.length;
+			$("h3").text("0/" + total_characters);
 
-		shuffle(characters);
-		refreshWords(characters);
+			shuffle(characters);
+			refreshWords(characters);
+		}
+		
 	});
 
 	// Quiz functionality
-	// Reveal the character
-	$("#reveal").click(function() {
-		$("#reveal").text(characters[0][2]);
-		$("#reveal").addClass('large-text');
+	// Reveal the hidden text
+	$("#reveal-pinyin").click(function() {
+		$("#reveal-pinyin").text(characters[0][0]);
+	});
+	$("#reveal-english").click(function() {
+		$("#reveal-english").text(characters[0][1]);
+	});
+	$("#reveal-character").click(function() {
+		$("#reveal-character").text(characters[0][2]);
+		$("#reveal-character").addClass('large-text');
 	});
 
 	completed_words = []; // for stats
@@ -90,10 +114,14 @@ $(document).ready(function() {
 
 	// Helper functions
 	function refreshWords(array) {
-		$("h1").text(characters[0][0]);
-		$("h2").text(characters[0][1]);
-		$("#reveal").text("Reveal");
-		$("#reveal").removeClass('large-text');
+		$("#reveal-pinyin").text(characters[0][0]);
+		$("#reveal-english").text(characters[0][1]);
+		$("#reveal-character").text(characters[0][2]);
+		$("#reveal-character").removeClass('large-text');
+		for (var i = 0; i < hidden.length; i++) {
+			$("#reveal-"+hidden[i].toLowerCase()).text("Reveal "+hidden[i]);
+			$("#reveal-"+hidden[i].toLowerCase()).addClass("light-btn");
+		};
 	};
 
 	function shuffle(array) {
